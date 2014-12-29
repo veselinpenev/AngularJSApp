e759a7fbabf424a9cb71c7d699ea8ed02d0ffc5c
Veselin Penev <veselinpenev@abv.bg>
2014-12-29 15:07:47 +0200

    Update All ads (add categories and tows)

--- app/js/controllers/MainController.js
+++ app/js/controllers/MainController.js
@@ -1,3 +1,11 @@
-adsModule.controller('MainController', function ($scope) {
-    $scope.name='name123';
+adsModule.controller('MainController', function ($scope, mainData) {
+    mainData.getAllAds(function (resp) {
+        $scope.data = resp;
+    });
+    mainData.getAllTowns(function (resp) {
+        $scope.allTowns = resp;
+    });
+    mainData.getAllCategories(function (resp) {
+        $scope.allCategories = resp;
+    });
 })
--- app/js/services/mainPage.js
+++ app/js/services/mainPage.js
@@ -1,7 +1,31 @@
-//adsModule.factory('mainData', function ($http) {
-//    return {
-//        getAllAds : function (success) {
-//
-//        }
-//    }
-//})
+adsModule.factory('mainData', function ($http, $log) {
+    return {
+        getAllAds : function (success) {
+            $http({method: 'GET', url:'http://localhost:1337/api/ads?pagesize=10&startpage=1'})
+                .success(function (data, status, headers, config) {
+                    success(data)
+                })
+                .error(function (data, status, headers, config) {
+                    $log.warn(data);
+                })
+        },
+        getAllTowns : function (success) {
+            $http({method: 'GET', url:'http://localhost:1337/api/towns'})
+                .success(function (data, status, headers, config) {
+                    success(data)
+                })
+                .error(function (data, status, headers, config) {
+                    $log.warn(data);
+                })
+        },
+        getAllCategories : function (success) {
+            $http({method: 'GET', url:'http://localhost:1337/api/categories'})
+                .success(function (data, status, headers, config) {
+                    success(data)
+                })
+                .error(function (data, status, headers, config) {
+                    $log.warn(data);
+                })
+        }
+    }
+})
